29961252ded63d549c7ad0ded88f13ec98b27c67
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-02-19 22:29:01 +0900

    Add files via upload

--- Lua 스터디_W1/view1.lua
+++ Lua 스터디_W1/view1.lua
@@ -10,30 +10,56 @@ local scene = composer.newScene()
 function scene:create( event )
 	local sceneGroup = self.view
 
+	--배경--
 	local background = display.newRect(display.contentWidth/2,display.contentHeight/2,
 										display.contentWidth,display.contentHeight)
 	background:setFillColor(1)
 
+	--연못--
 	local pond = display.newCircle(display.contentWidth*0.6,display.contentHeight/2,350)
 	pond: setFillColor(0.5,0.8,1)
 
+	--물고기소환--
 	local fish = {}
 	local fishGroup = display.newGroup()
-
+	--랜덤--
 	for i = 1, 5 do
 		fish[i] = display.newImage(fishGroup,"image/fish.png")
-		fish[i].x,fish[i].y = pond.x+ 50*i, pond.y+50*i
+		fish[i].x,fish[i].y = pond.x+ math.random(-200,200), pond.y+math.random(-200,200)
 	end
-	fishGroup.x=fishGroup.x-150
-	fishGroup.y=fishGroup.y-150
 
+	--고양이소환--
 	local cat = display.newImageRect("image/cat.png", 150*1.2,200*1.2)
 	cat.x,cat.y=display.contentWidth*0.4, display.contentHeight*0.8
-
+	--점수--
 	local score=0
 	local showScore=display.newText(score,display.contentWidth*0.2,display.contentHeight*0.2)
 	showScore:setFillColor(0)
 	showScore.size=100
+
+	--탭이벤트--
+	local function catch(event)
+		display.remove(event.target)
+		score = score + 1
+		showScore.text = score
+
+		if score==5 then
+			composer.gotoScene("view2")
+		end
+	end
+
+	for i=1,5 do
+		fish[i]:addEventListener("tap",catch)
+	end
+
+	--레이어 정리 순서 중요--
+	sceneGroup:insert(background)
+	sceneGroup:insert(fishGroup)
+	sceneGroup:insert(pond)
+	sceneGroup:insert(cat)
+	sceneGroup:insert(showScore)
+
+	fishGroup:toFront()
 end
 
 
@@ -62,6 +88,7 @@ function scene:hide( event )
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
+		composer.removeScene("view1")
 	end
 end
 
--- Lua 스터디_W1/view2.lua
+++ Lua 스터디_W1/view2.lua
@@ -10,49 +10,11 @@ local scene = composer.newScene()
 function scene:create( event )
 	local sceneGroup = self.view
 	
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
-	
-	-- create a white background to fill screen
-	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
-	background:setFillColor( 1 )	-- white
-	
-	-- create some text
-	local title = display.newText( "Second View", display.contentCenterX, 125, native.systemFont, 32 )
-	title:setFillColor( 0 )	-- black
+	local TEXT = display.newText("CLEAR",display.contentWidth*0.5,display.contentHeight*0.5)
+	TEXT:setFillColor(1)
+	TEXT.size = 200
 
-	local newTextParams = { text = "Loaded by the second tab's\n\"onPress\" listener\nspecified in the 'tabButtons' table", 
-							x = display.contentCenterX + 10, 
-							y = title.y + 215, 
-							width = 310, 
-							height = 310, 
-							font = native.systemFont, 
-							fontSize = 14, 
-							align = "center" }
-	local summary = display.newText( newTextParams )
-	summary:setFillColor( 0 ) -- black
-	
-	-- all objects must be added to group (e.g. self.view)
-	sceneGroup:insert( background )
-	sceneGroup:insert( title )
-	sceneGroup:insert( summary )
-end
-
-function scene:show( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if phase == "will" then
-		-- Called when the scene is still off screen and is about to move on screen
-	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
-	end	
-end
+end 
 
 function scene:hide( event )
 	local sceneGroup = self.view
